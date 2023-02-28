b4ec7143dc693a7c3d0cab053b74e47302e07022
Tumbichi <germanviglietti@gmail.com>
2023-02-27 23:14:55 -0300

    feat: add loading provider on app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,15 @@ import type { AppProps } from "next/app";
 import "@/Core/styles/globals.css";
 
 import MainLayout from "@/Core/layouts/MainLayout";
+import useRouteLoading from "@/Core/hooks/useRouteLoading";
+import LoadingProvider from "@/Core/contexts/LoadingContext/LoadingProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <LoadingProvider>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </LoadingProvider>
   );
 }
